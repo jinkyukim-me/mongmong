3c2ab54399113f6a251c90f52b15300c42fc53b9
biniruu <biniruu@gmail.com>
2019-12-10 23:02:35 +0900

    아이콘 변경

--- DailySummary/client/src/components/Post/Write/Emotion.js
+++ DailySummary/client/src/components/Post/Write/Emotion.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Icon } from 'antd'
-import { faTired, faFrownOpen, faMeh, faSmile, faLaughSquint } from "@fortawesome/free-regular-svg-icons";
+import { faTired, faFrownOpen, faMeh, faSmile, faLaughBeam } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Emotion extends Component {
@@ -10,24 +10,19 @@ class Emotion extends Component {
         <p className="emotion-describe">오늘 기분은??</p>
         <div className='iconList'>
           <Button value="4" onClick={this.props.clickHandler}>
-            {/* <Icon type="like" /> */}
-            <FontAwesomeIcon icon={faTired} className="icon" />
+            <FontAwesomeIcon icon={faLaughBeam} className="icon" />
           </Button>
           <Button value="3" onClick={this.props.clickHandler}>
-            {/* <Icon type="smile" /> */}
-            <FontAwesomeIcon icon={faFrownOpen} className="icon" />
+            <FontAwesomeIcon icon={faSmile} className="icon" />
           </Button>         
           <Button value="2" onClick={this.props.clickHandler}>
-           {/* <Icon type="meh" /> */}
            <FontAwesomeIcon icon={faMeh} className="icon" />
           </Button>
           <Button value="1" onClick={this.props.clickHandler}>
-            {/* <Icon type="frown" /> */}
-            <FontAwesomeIcon icon={faSmile} className="icon" />
+            <FontAwesomeIcon icon={faFrownOpen} className="icon" />
           </Button>         
           <Button value="0" onClick={this.props.clickHandler}>
-            {/* <Icon type="dislike" /> */}
-            <FontAwesomeIcon icon={faLaughSquint} className="icon" />
+            <FontAwesomeIcon icon={faTired} className="icon" />
           </Button>        
         </div>
       </div>
@@ -36,38 +31,3 @@ class Emotion extends Component {
 }
 
 export default Emotion
-
-// import React, { Component } from 'react'
-// import { Button, Icon } from 'antd'
-// import axios from 'axios';
-
-// class Emotion extends Component {
-  
-
-//   render() {
-//     return(
-//       <div className="App-Content-Emotion">
-//         <p>오늘 기분은??</p>
-//         <div className='iconList' style={{ padding: 5, background: '#fff', minHeight: 20}}>
-//           <Button >
-//             <Icon type="like" />
-//           </Button>
-//           <Button >
-//             <Icon type="smile" />
-//           </Button>         
-//           <Button >
-//            <Icon type="meh" />
-//           </Button>
-//           <Button >
-//             <Icon type="frown" />
-//           </Button>         
-//           <Button >
-//             <Icon type="dislike" />
-//           </Button>        
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default Emotion
